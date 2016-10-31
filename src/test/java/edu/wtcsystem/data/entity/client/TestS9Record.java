d4cd97541867262e86c1fa736384e22d3c7dda97
Christian Winebrenner <jadecristal@gmail.com>
2016-10-31 13:45:31 -0500

    Fixed .gitignore for src/test, src/main/resources, README.md

---
+++ src/test/java/edu/wtcsystem/data/entity/client/TestS9Record.java
@@ -0,0 +1,23 @@
+package edu.wtcsystem.data.entity.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author cwinebrenner
+ * @author nataliya.knudson
+ * @since 2016-10-31
+ */
+public class TestS9Record {
+
+    @Test
+    public void testS9RecordCreate() {
+        //TODO: Make this create a S9 record
+    }
+
+    @Test
+    public void testS9RecordIsValid() {
+
+    }
+}
